c56a75e191501fe56beef1263c894c093d85d89f
Scott Scoble <sscoble@codesmith.ws>
2015-03-26 22:34:42 -0700

    trap exit for docker restart to work properly

--- start.sh
+++ start.sh
@@ -2,4 +2,17 @@
 chown www-data: -R /var/www/laravel
 env | grep _ >> /etc/environment
 source /etc/apache2/envvars
+
+LOCKFILE=/run/apache2/apache2.pid
+
+# Previous apache should execute successfully:
+
+[ -f $LOCKFILE ] && exit 0
+
+# Upon exit, remove lockfile.
+
+trap "{ rm -f $LOCKFILE ; exit 255; }" EXIT
+
 /usr/sbin/apache2 -D FOREGROUND
+
+exit 0
